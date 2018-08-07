7f75436f09b2f42a4e2da6748d9f567e985124db
Connor Sweeney <cs293407@gmail.com>
2018-08-07 15:37:17 -0400

    Implement handleClick and pass props down to Board in Game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,20 +1,55 @@
 import React from 'react';
 import Board from '../Board/Board';
+import calculateWinner from '../Board/BoardHelper';
 
-const Game = () => (
-  <div className="game">
-    <div className="game-board">
-      <Board />
-    </div>
-    <div className="game-info">
-      <div>
-        {/* status */}
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  getNextPlayer() {
+    const { xIsNext } = this.state;
+    return xIsNext ? 'X' : 'O';
+  }
+
+  handleClick(i) {
+    const { squares, xIsNext } = this.state;
+    const winner = calculateWinner(squares);
+    if (squares[i] == null && !winner) {
+      const updatedSquares = squares.slice();
+      updatedSquares[i] = this.getNextPlayer();
+      this.setState({
+        squares: updatedSquares,
+        xIsNext: !xIsNext,
+      });
+    }
+  }
+
+  render() {
+    const { squares } = this.state;
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>
+            {/* status */}
+          </div>
+          <ol>
+            {/* TODO */}
+          </ol>
+        </div>
       </div>
-      <ol>
-        {/* TODO */}
-      </ol>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Game;
--- src/components/Game/Game.spec.js
+++ src/components/Game/Game.spec.js
@@ -1,11 +1,81 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import calculateWinner from '../Board/BoardHelper';
 import Game from './Game';
+import Board from '../Board/Board';
+
+jest.mock('../Board/BoardHelper');
 
 describe('Game', () => {
-  it('exists', () => {
-    const wrapper = shallow(<Game />);
+  let wrapper;
 
-    expect(wrapper).not.toBeUndefined();
+  beforeEach(() => {
+    calculateWinner.mockImplementation(() => null);
+    wrapper = shallow(<Game />);
+  });
+
+  afterEach(() => {
+    calculateWinner.mockRestore();
+  });
+
+  it('initializes squares to nulls', () => {
+    const expectedSquares = [null, null, null, null, null, null, null, null, null];
+    expect(wrapper.state('squares')).toEqual(expectedSquares);
+  });
+
+  it('initializes next player to X', () => {
+    expect(wrapper.state('xIsNext')).toBe(true);
+  });
+
+  it('passes squares to Board', () => {
+    const squares = ['X', null, 'X', null, 'O', null, 'X', null, 'O'];
+    wrapper.setState({ squares });
+
+    expect(wrapper.find(Board).prop('squares')).toBe(squares);
+  });
+
+  it('passes onClick to Board', () => {
+    expect(wrapper.find(Board).prop('onClick')).not.toBeUndefined();
+  });
+
+  describe('handleClick', () => {
+    it('sets the given index in state.squares to X if next player is X', () => {
+      wrapper.instance().handleClick(8);
+
+      expect(wrapper.state('squares')[8]).toBe('X');
+    });
+
+    it('sets the given index in state.squares to O if next player is O', () => {
+      wrapper.setState({ xIsNext: false });
+
+      expect(wrapper.state('xIsNext')).toBe(false);
+      wrapper.instance().handleClick(8);
+
+      expect(wrapper.state('squares')[8]).toBe('O');
+    });
+
+    it('toggles which player moves next', () => {
+      wrapper.instance().handleClick(8);
+
+      expect(wrapper.state('xIsNext')).toBe(false);
+    });
+
+    it('does not update square if square already has a value', () => {
+      const squares = [null, null, null, null, null, null, null, null, 'O'];
+      wrapper.setState({ squares });
+
+      wrapper.instance().handleClick(8);
+
+      expect(wrapper.state('squares')[8]).toBe('O');
+    });
+
+    it('does not update square if a player has won', () => {
+      calculateWinner.mockImplementation(() => 'X');
+      wrapper = shallow(<Game />);
+
+      wrapper.instance().handleClick(8);
+
+      expect(wrapper.state('squares')[8]).toBe(null);
+    });
   });
 });
